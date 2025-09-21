7ae0b54fd65a979413496bd280e12e1b34589c50
Your Name <you@example.com>
2025-09-21 00:22:15 +0000

    Fix filename and import App.jsx for deployment

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import App from './App.jsx';
+import App from './App';
 import './App.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
