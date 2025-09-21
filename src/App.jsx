feef74725c79d3da350b7b75c925f000e4fc51bc
Your Name <you@example.com>
2025-09-21 15:48:56 +0000

    mise à jour du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-import './App.css';
+import './app.css';
 
 const BackgroundAnimation = () => {
   return (
