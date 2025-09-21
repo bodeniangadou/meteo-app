8e585fa67184ebd8b7bb91112ec92be8f8166f12
Your Name <you@example.com>
2025-09-21 15:47:10 +0000

    mise à jour du code

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import App from './App';
-import './App.css';
+import './app.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
